903d6f6ad08c751c98324d2818180ade8e67a080
Esemega <sogaral89@gmail.com>
2020-10-01 17:51:43 +0200

    initial plan

---
+++ app.js
@@ -0,0 +1,10 @@
+//initial data structure
+const product = { count: 3, price: 12.55, type: "ropa" };
+
+//calculate the VAT
+
+//switch per product.type
+
+//calculate totalVat
+
+//calculate totalPrice taking in acccount the conditions
